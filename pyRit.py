b543cb4accb98a5450128e99847f10d634eb7d3e
Theoretical <mdeception@gmail.com>
2014-02-07 09:14:37 -0500

    Removed the terrible throttling and added WSGI for Flask

--- pyRit.py
+++ pyRit.py
@@ -1,3 +1,4 @@
+from gevent.wsgi import WSGIServer
 from flask import Flask, jsonify, Response
 from json import dumps
 from multiprocessing import Process, Pipe
@@ -28,29 +29,6 @@ class pyRit:
         self.app.add_url_rule('/game/<summonerName>', 'game', self.getGameInProgress)
         self.app.add_url_rule('/ranked/<summoner>/<season>', 'ranked', self.getRankedStats, defaults={'season': 'CURRENT'})
         self.app.add_url_rule('/leagues/<summoner>', 'leagues', self.getAllLeaguesForPlayer)
-        self.app.before_request(self.processThrottle)
-
-    def _throttleThread(self):
-        start = time()
-        REQUEST_THROTTLE = 500
-
-        while True:
-            if start <= time() + 5:
-                start = time()
-                if self.requests > REQUEST_THROTTLE and self.clientIndex < 50:
-                    c = LolClient(self.region, '{0}{1}'.format(self.userKey, self.clientIndex), self.password, self.version)
-                    p_in, p_out = Pipe()
-                    p = Process(target=ClientInstance, args=(c, p_out), name='pyRit-{0}-{1}{2}'.format(self.region, self.userKey, self.clientIndex))
-                    p.start()
-                    self.clients.put_nowait(p_in)
-                    self.clientIndex += 1
-
-                    print 'Launched client {0} due to throttle'.format(self.clientIndex)
-                    self.requests = 0
-            sleep(.5)
-
-    def processThrottle(self):
-        self.requests += 1
 
     def createClients(self, clientCount):
         self.clients = Queue()
@@ -66,8 +44,8 @@ class pyRit:
 
     def start(self, clientCount, debug=False):
         self.createClients(clientCount)
-        Thread(target=self._throttleThread).start()
-        self.app.run(host='0.0.0.0', threaded=True, debug=debug)
+        server = WSGIServer(('', 5000), self.app)
+        server.serve_forever()
 
     def nextClient(self):
         c = self.clients.get(False)
